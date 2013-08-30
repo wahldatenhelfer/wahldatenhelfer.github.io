489c67bf39c89c47ae85c0c8cc28751d4d6fbbec
Tatjana Lajendäcker <tatjana@railslove.com>
2013-08-30 18:23:42 +0200

    added new map style

--- javascript.js
+++ javascript.js
@@ -1,2 +1,35 @@
 $(function() {
+
+	  mapKoeln = new google.maps.Map(document.getElementById('map-koeln'), {
+		  zoom: 16,
+	    center: new google.maps.LatLng(50.92220965993466, 6.96285226387031),
+	    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    disableDefaultUI: true,
+	    draggable: false,
+	   	scrollwheel: false
+	  });
+
+    var marker_koeln = new google.maps.Marker({
+      position: new google.maps.LatLng(50.92220965993466, 6.96285226387031),
+      map: mapKoeln,
+      title: 'Wahllokal'
+    });
+
+	  mapGermany = new google.maps.Map(document.getElementById('map-germany'), {
+		  zoom: 10,
+	    center: new google.maps.LatLng(50.92220965993466, 6.96285226387031),
+	    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    disableDefaultUI: true,
+	    draggable: false,
+	   	scrollwheel: false
+	  });
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(50.92220965993466, 6.96285226387031),
+      map: mapGermany,
+      title: 'Wahllokal'
+    });
+
 });
+
+
